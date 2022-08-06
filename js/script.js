2be57283ff50f2124cf0fc006ba17a5a59109786
Albert Sakhibullin <sakhibullin2004@mail.ru>
2022-08-06 17:24:36 +0300

    2

--- js/script.js
+++ js/script.js
@@ -128,47 +128,47 @@ document.querySelectorAll('.burger')[0].addEventListener('click',function(){
   document.querySelectorAll('.menu')[0].classList.toggle('none')
 })
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  // direction: 'horizontal',
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 0
-    },
-    // when window width is >= 480px
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
-    // when window width is >= 640px
-    1251: {
-      slidesPerView: 4,
-      spaceBetween: 30,
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   // direction: 'horizontal',
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 1,
+//       spaceBetween: 0
+//     },
+//     // when window width is >= 480px
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 30
+//     },
+//     // when window width is >= 640px
+//     1251: {
+//       slidesPerView: 4,
+//       spaceBetween: 30,
       
-    }
-  },
+//     }
+//   },
 
-  slidesPerView: 4,
-  loop: true,
-  speed: 400,
-  spaceBetween: 30,
-  allowTouchMove:true,
-  autoplay: {
-    delay: 5000,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    dragSize:158,
-  },
-  });
+//   slidesPerView: 4,
+//   loop: true,
+//   speed: 400,
+//   spaceBetween: 30,
+//   allowTouchMove:true,
+//   autoplay: {
+//     delay: 5000,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//     dragSize:158,
+//   },
+//   });
 
-}
+// }
 
 
 
